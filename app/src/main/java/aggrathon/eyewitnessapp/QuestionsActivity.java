8c4027cbcdeeb5353034dfc574e8b23b3b5d338a
Aggrathon <antonbjo@gmail.com>
2017-05-14 01:39:31 +0300

    Seekbar text indicators

--- app/src/main/java/aggrathon/eyewitnessapp/QuestionsActivity.java
+++ app/src/main/java/aggrathon/eyewitnessapp/QuestionsActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -13,6 +15,32 @@ public class QuestionsActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_questions);
+
+		final TextView sureText = (TextView)findViewById(R.id.sureText);
+		((SeekBar)findViewById(R.id.sureBar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+				sureText.setText(i+"%");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}
+		});
+
+		final TextView distText = (TextView)findViewById(R.id.distText);
+		((SeekBar)findViewById(R.id.distBar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+				distText.setText(i+" m");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}
+		});
 	}
 
 }
