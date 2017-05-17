ef2f24999f7fcbe80f8774882d7242bce703734e
Aggrathon <antonbjo@gmail.com>
2017-05-18 02:37:17 +0300

    Settings Mockup

--- app/src/main/java/aggrathon/eyewitnessapp/MainActivity.java
+++ app/src/main/java/aggrathon/eyewitnessapp/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
 		StorageManager.checkForStoragePermissions(this);
 		Log.d("Main",""+System.currentTimeMillis());
+
+		//Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+		//setSupportActionBar(tb);
 	}
 
 	public void onLangSwedish(View view) {
@@ -53,5 +60,22 @@ public class MainActivity extends AppCompatActivity {
 		if (requestCode == StorageManager.STORAGE_PERMISSION_REQUEST) {
 			StorageManager.handleStoragePermissionCallback(this, grantResults[0] == PackageManager.PERMISSION_GRANTED);
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.action_settings) {
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		}
+		else
+			return super.onOptionsItemSelected(item);
 	}
 }
