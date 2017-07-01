9f0337f1504c064c5b6652ad8561a45bae35f216
Aggrathon <antonbjo@gmail.com>
2017-07-02 00:33:21 +0300

    Small tweaks and fixes

--- app/src/main/java/aggrathon/eyewitnessapp/start/ExperimentInformationActivity.java
+++ app/src/main/java/aggrathon/eyewitnessapp/start/ExperimentInformationActivity.java
@@ -41,7 +41,7 @@ public class ExperimentInformationActivity extends AImmersiveActivity {
 	}
 
 	public void onNextButton(View v) {
-		ExperimentData.getInstance().personalInformation.previousParticipations = !prevPartButton.isChecked();
+		ExperimentData.getInstance().personalInformation.previousParticipations = prevPartButton.isChecked();
 		startActivity(new Intent(this, BackgroundInformationActivity.class));
 		finish();
 	}
